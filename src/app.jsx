38cc6967b9ae8ff880677859c5d535e6485f17bc
brayd <bdwalk00@byu.edu>
2024-11-09 14:46:26 -0700

    trying to figure out login authentications

--- src/app.jsx
+++ src/app.jsx
@@ -1,49 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Game } from './game/game';
 import { Score } from './score&info/score';
+import { AuthState } from './login/authState';
 
 export default function App() {
+  const [authState, setAuthState] = useState(AuthState.Unknown);
+  const [userName, setUserName] = useState('');
+
   return (
     <BrowserRouter>
-    <div className="bg-dark text-success d-flex flex-column min-vh-100">
+      <div className="bg-dark text-success d-flex flex-column min-vh-100">
         <header className="py-3 border-bottom border-success">
-            <div className="container text-center">
-                <h1>Binary Blast</h1>
-            </div>
-            <nav>
-                <menu>
-                    <ul className="nav justify-content-center">
-                        <li className="nav-item"><NavLink className="nav-link text-success" href=''>Login</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link text-success" to='game'>Play</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link text-success" to='score'>Scores & learning</NavLink></li>
-                    </ul>
-                </menu>
-            </nav>
+          <div className="container text-center">
+            <h1>Binary Blast</h1>
+          </div>
+          <nav>
+            <menu>
+              <ul className="nav justify-content-center">
+                <li className="nav-item"><NavLink className="nav-link text-success" to=''>Login</NavLink></li>
+                {authState === AuthState.Authenticated && (
+                  <li className="nav-item"><NavLink className="nav-link text-success" to='game'>Play</NavLink></li>
+                )}
+                {authState === AuthState.Authenticated && (
+                  <li className="nav-item"><NavLink className="nav-link text-success" to='score'>Scores & learning</NavLink></li>
+                )}
+              </ul>
+            </menu>
+          </nav>
         </header>
 
         <Routes>
-            <Route path='/' element={<Login />} exact />
-            <Route path='/game' element={<Game />} />
-            <Route path='/score' element={<Score />} />
-            <Route path='*' element={<NotFound />} />
+          <Route
+            path='/'
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            }
+            exact
+          />
+          <Route path='/game' element={<Game />} />
+          <Route path='/score' element={<Score />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
 
         <footer className="mt-auto py-3 border-top border-success">
-            <div className="container text-center">
-                <span className="text-reset">Brayden Walker</span>
-                <br />
-                <a href="https://github.com/BraydenReklaw/startup.git" className="text-success">GitHub</a>
-            </div>
+          <div className="container text-center">
+            <span className="text-reset">Brayden Walker</span>
+            <br />
+            <a href="https://github.com/BraydenReklaw/startup.git" className="text-success">GitHub</a>
+          </div>
         </footer>
-    </div>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
 function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
-  }
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
 
-export function Login() {
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className="container my-5 text-center flex-grow-1">
-      <h1>Welcome, User, to Binary Blast</h1>
+      {/* <h1>Welcome, User, to Binary Blast</h1>
             <form method="get" action="game.html" className="my-4">
                 <div className="mb-3">
                     <input type="email" className="form-control text-success border-success" placeholder="your@email.com" />
@@ -13,7 +16,19 @@ export function Login() {
                 </div>
                 <button type="submit" className="btn btn-outline-success">Login</button>
                 <button type="submit" className="btn btn-outline-success">Create</button>
-            </form>
+            </form> */}
+        <div>
+        {authState !== AuthState.Unknown && <h1>Welcome to Binary Blast</h1>}
+        {authState === AuthState.Authenticated && <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+        </div>
     </main>
   );
 }
