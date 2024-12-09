03cdf3737cdceee8aa965e945167402337c177d9
BraydenW <braydenthewalker@gmail.com>
2024-12-09 16:43:32 -0700

    updated score with correct table indexing, implemented simon's aunthentications

--- src/score&info/score.jsx
+++ src/score&info/score.jsx
@@ -37,7 +37,7 @@ export function Score() {
         for (const [i, score] of scores.entries()) {
         scoreRows.push(
             <tr key={i}>
-                <td>{i}</td>
+                <td>{i+1}</td>
                 <td>{score.name.split('@')[0]}</td>
                 <td>{score.score}</td>
                 <td>{score.date}</td>
